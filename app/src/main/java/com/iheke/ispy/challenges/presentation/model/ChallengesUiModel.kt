2a95d9e266cd1b0802d7167d258ee74503701d00
Nnenna Iheke <nniheke@gmail.com>
2023-06-10 05:35:59 -0400

    Initial Commit

--- app/src/main/java/com/iheke/ispy/challenges/presentation/model/ChallengesUiModel.kt
+++ app/src/main/java/com/iheke/ispy/challenges/presentation/model/ChallengesUiModel.kt
@@ -1,8 +1,17 @@
 package com.iheke.ispy.challenges.presentation.model
 
+/**
+ * Represents the UI model for challenges.
+ *
+ * @property wins The number of wins for the challenge.
+ * @property rating The average rating for the challenge.
+ * @property hint The hint for the challenge.
+ * @property image The image associated with the challenge.
+ */
 data class ChallengesUiModel(
     val wins: Int,
     val rating: Double,
     val hint: String,
     val image: String
 )
+
